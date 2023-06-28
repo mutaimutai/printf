440bcabf26dd1abec641e51d5b1bb89effb75e8c
mutaimutai <engineermutai75@gmail.com>
2023-06-28 03:10:22 -0700

    printf

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <limits.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	_printf("%c%cth %s%s a%cg%s: Y%sou %s no%ching%s Snow.%c", 'W', 'i', "some ", "more", 'r', "s", "", "know", 't', ", Jon", '\n');
+  return (0);
+}
