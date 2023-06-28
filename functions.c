15392313229a732fe00e20edccbe60bd46245e78
mutaimutai <engineermutai75@gmail.com>
2023-06-27 19:58:59 -0700

    printf

--- functions.c
+++ functions.c
@@ -43,8 +43,39 @@ int print_str(va_list any)
  * percent_sign-prints %
  * Return:returns 1
  */
-int percent_sign()
+int percent_sign(void)
 {
 	write(1, "%", 1);
 	return (1);
+}
+/**
+ * int print_decimal-prints an integer
+ * @any:arguments from variable list
+ * Return: number of printed characters
+ */
+int print_decimal(va_list any)
+{
+int i = 0;
+	int characters = 0;
+	int out;
+	int arr[10];
+	char caleb[1];
+
+	out = va_arg(args, int);
+
+	while (numb != 0)
+	{
+		arr[i] = (out % 10);
+		out = out / 10;
+		if (out == 0)
+			return -1;
+	}
+
+	for (i; i >= 0; i--)
+	{
+		caleb[0] = ('0' + arr[i]);
+		out += write(1, x, 1);
+	}
+
+	return (out);
 }
