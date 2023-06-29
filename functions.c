609f66d7edecb46aac31076ca147b059d328f07c
fredmugambi <80039283+fredmugambi@users.noreply.github.com>
2023-06-29 14:37:26 +0300

    Update functions.c

--- functions.c
+++ functions.c
@@ -140,3 +140,46 @@ int print_integer(va_list any)
 		return (characters);
 	}
 }
+/**
+*print binary-prints Unsigned number
+*@t-list of arguments
+*@b-buffers array to hande the print
+*@f-used to calculate the flags that are active
+*@w-get the width
+*@p-the specification of precision
+*@s-specifiers size
+*@Return - this returns number of characters that will be printed
+*/
+int print_binary(va_list types,char b[],int f,int w,int p,int s)
+{
+	unsigned int n,m,i,sum;
+unsigned int a[32];
+int count;
+
+UNUSED(b);
+UNUSED(f);
+UNUSED(w);
+UNUSED(p);
+UNUSED(s);
+
+n=va_arg(types,unsigned int);
+m=2147483648;
+/*(2^31)*/
+a[0]=n/m;
+for (i=1;i<32;i++)
+{
+m/=2;
+a[i]=(n/m)%2;
+}
+for (i=0,sum=0,count=0;i<32;i++)
+{
+sum +=a[i];
+if(sum || i==31)
+{
+char z='0' + a[i];
+write (1,&z,1);
+count++;
+}
+}
+return(count);
+}
